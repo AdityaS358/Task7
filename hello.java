e315db7ed15d7652c2587dbadc00de5c5f2c96fa
AdityaS358 <130598697+AdityaS358@users.noreply.github.com>
2024-04-01 00:30:29 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class hello{
+    public static void main(String args[]){
+        System.out.print("Hellooo from GitHUB!!!!!");
+        System.out.print("Hellooo from Docker!!!!!");
+    }
+}
